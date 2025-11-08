b8bcd853ff51d6817d3e8ee2f5491338f3a20bd1
Pedrozaz <guilhermebarb0sa@proton.me>
2025-11-08 17:00:09 -0300

    Initial commit

---
+++ src/main/java/io/github/pedrozaz/securenotesservice/SecureNotesServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.pedrozaz.securenotesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureNotesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureNotesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/pedrozaz/securenotesservice/SecureNotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedrozaz.securenotesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureNotesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
